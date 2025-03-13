92fc48166a2e08b00068da0f679bca541c05a5e0
Kemo Krpo <162745688+kemokrpo@users.noreply.github.com>
2025-03-13 20:19:35 +0100

    Small fix

--- src/components/MilestoneBar/MilestoneBar.tsx
+++ src/components/MilestoneBar/MilestoneBar.tsx
@@ -5,19 +5,19 @@ const MilestoneBar: FC = () => {
     <section className="h-[60vh] mt-20 mb-20 flex justify-center bg-gray-100 dark:bg-dbg-dark pb-10 relative">
       <ol className="relative border-l-2 border-main-700 flex flex-col content-center">
         <li className="mb-[8rem] ml-10">
-          <span className="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8 ring-main-700 bg-gray-100"></span>
+          <span className="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8 ring-main-700 bg-gray-100 dark:bg-dbg-dark"></span>
           <h3 className="flex items-center mb-1 text-md  text-gray-600 ">
             Osnivanje lokalne grupe BEST Mostar - 2011
           </h3>
         </li>
         <li className="mb-[8rem] ml-10">
-          <span className="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8 ring-main-700 bg-gray-100"></span>
+          <span className="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8 ring-main-700 bg-gray-100 dark:bg-dbg-dark"></span>
           <h3 className="flex items-center mb-1 text-md  text-gray-600 ">
             Status baby clana lokalne grupe - 2011
           </h3>
         </li>
         <li className="mb-10 ml-10">
-          <span className="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8 ring-main-700 bg-gray-100"></span>
+          <span className="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8 ring-main-700 bg-gray-100 dark:bg-dbg-dark"></span>
           <h3 className="flex items-center mb-1 text-md  text-gray-600 ">
             Status punopravnog clana lokalne grupe - 2011
           </h3>
