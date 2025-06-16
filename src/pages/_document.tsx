e5e213ee8ea221193dc48edb26304511d02e014d
Kemo Krpo <krpokemo@gmail.com>
2025-06-16 16:35:56 +0200

    Name

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+       <Head>
+        <title>BEST Mostar</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="BEST Mostar official website" />
+      </Head>
       <body>
         <Main />
         <NextScript />
